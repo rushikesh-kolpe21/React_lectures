835acf87e745eb3b847fe962a899e63283ecc167
Rushikesh kolpe <155113038+rushikesh-kolpe21@users.noreply.github.com>
2025-11-29 09:51:33 +0530

    your new update

--- React_bun/src/App.jsx
+++ React_bun/src/App.jsx
@@ -1,21 +1,12 @@
+import { Netflix } from "./components/Netflix";
+
  export const App =()=> {
+
+  const age = 17;
+
   return(
     <>
-    <div>
-       <h1>Shree Ganesh</h1>
-
-     
-      <img src="money_image.jpg" alt="honey hest"
-      width="39%" height="39%" />
-     </div>
-
-     <h2>Name: Money Heistgfv</h2>
-     <h3>Rating : 5</h3>
-     <h4>Summer: Lorem ipsum dolor, sit amet consectetur 
-      adipisicing elit. Architecto aperiam in veniam similique,
-       tempore vel atque expedita sed. Atque impedit eum vel corporis 
-      perspiciatis nemo ex blanditiis cum in facilis!</h4>
-  
+    <Netflix/>
     </>
   )
 }                                                          
